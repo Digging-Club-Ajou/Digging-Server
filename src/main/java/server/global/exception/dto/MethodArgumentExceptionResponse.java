2f16b6d608c8a329f914ce20a0aa4e7ce62652de
Jung-YouHwan <yhwjd99@gmail.com>
2023-09-13 10:53:49 +0900

    예외 처리 Response 및 ApiRestControllerAdvice 생성

--- src/main/java/server/global/exception/dto/MethodArgumentExceptionResponse.java
+++ src/main/java/server/global/exception/dto/MethodArgumentExceptionResponse.java
@@ -1,26 +1,13 @@
 package server.global.exception.dto;
 
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 import org.springframework.validation.FieldError;
 
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
-@Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class MethodArgumentExceptionResponse {
-
-    private String statusCode;
-
-    private Map<String, String> validation = new ConcurrentHashMap<>();
-
-    @Builder
-    private MethodArgumentExceptionResponse(final String statusCode) {
-        this.statusCode = statusCode;
-    }
+public record MethodArgumentExceptionResponse(
+        String statusCode,
+        Map<String, String> validation
+) {
 
     public void addValidation(final FieldError fieldError) {
         validation.put(fieldError.getField(), fieldError.getDefaultMessage());
