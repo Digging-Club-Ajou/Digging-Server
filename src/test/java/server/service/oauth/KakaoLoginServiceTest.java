1f76340494a5200d16befa9a1f6719722a411a6a
Jung-YouHwan <yhwjd99@gmail.com>
2023-09-18 14:40:08 +0900

    oauth 카카오 로그인 애플리케이션 로직 테스트 작성

---
+++ src/test/java/server/service/oauth/KakaoLoginServiceTest.java
@@ -0,0 +1,78 @@
+package server.service.oauth;
+
+import jakarta.servlet.http.Cookie;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockHttpServletResponse;
+import server.domain.member.persist.Member;
+import server.domain.member.vo.Gender;
+import server.global.constant.LoginConstant;
+import server.mapper.member.dto.KakaoSignupRequest;
+import server.util.ServiceTest;
+import server.util.TestConstant;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static server.global.constant.LoginConstant.*;
+import static server.util.TestConstant.*;
+
+class KakaoLoginServiceTest extends ServiceTest {
+
+    @Autowired
+    private KakaoLoginService kakaoLoginService;
+
+    @Test
+    @DisplayName("이미 가입된 회원은 회원가입 없이 바로 로그인을 합니다")
+    void alreadyKakaoLogin() {
+        // given 1
+        memberRepository.save(Member
+                .builder()
+                .email(TEST_EMAIL.value)
+                .build()
+        );
+
+        // given 2
+        KakaoSignupRequest dto = KakaoSignupRequest.builder()
+                .email(TEST_EMAIL.value)
+                .phoneNumber(TEST_PHONE_NUMBER.value)
+                .gender(Gender.UNKNOWN)
+                .build();
+
+        MockHttpServletResponse response = new MockHttpServletResponse();
+
+        // when
+        Member member = kakaoLoginService.kakaoLogin(dto, response);
+        Cookie cookie = response.getCookie(REFRESH_TOKEN.value);
+
+        // then
+        assertThat(memberRepository.count()).isEqualTo(1);
+        assertThat(cookie).isNotNull();
+        assertThat(cookie.getName()).isEqualTo(REFRESH_TOKEN.value);
+        assertThat(response.getHeader(ACCESS_TOKEN.value)).isNotNull();
+    }
+
+    @Test
+    @DisplayName("카카오 정보를 받아와서 회원 가입 후 로그인을 합니다")
+    void kakaoLogin() {
+        // given
+        KakaoSignupRequest dto = KakaoSignupRequest.builder()
+                .email(TEST_EMAIL.value)
+                .phoneNumber(TEST_PHONE_NUMBER.value)
+                .gender(Gender.UNKNOWN)
+                .build();
+
+        MockHttpServletResponse response = new MockHttpServletResponse();
+
+        // when
+        Member member = kakaoLoginService.kakaoLogin(dto, response);
+        Cookie cookie = response.getCookie(REFRESH_TOKEN.value);
+
+        // then
+        assertThat(memberRepository.count()).isEqualTo(1);
+        assertThat(cookie).isNotNull();
+        assertThat(cookie.getName()).isEqualTo(REFRESH_TOKEN.value);
+        assertThat(response.getHeader(ACCESS_TOKEN.value)).isNotNull();
+    }
+}
