a74b6a7201d8ea43c6e1bb8e2b4fbe3c06c22068
yeiin <yeini0417@gmail.com>
2023-10-08 18:49:18 +0900

    genre clean

--- src/main/java/server/controller/genre/GenreController.java
+++ src/main/java/server/controller/genre/GenreController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import server.domain.member.vo.MemberSession;
+import server.global.annotation.Login;
 import server.mapper.genre.dto.GenreRequest;
 import server.service.genre.GenreService;
 
@@ -19,9 +21,9 @@ public class GenreController {
     }
 
     @PostMapping("/genres")
-    public void createGenre(@RequestBody GenreRequest genreRequest) throws JsonProcessingException {
+    public void createGenre(@Login final MemberSession memberSession, @RequestBody GenreRequest genreRequest) throws JsonProcessingException {
         System.out.println(genreRequest.toString());
-        genreService.saveUserGenre(genreRequest);
+        genreService.saveUserGenre(memberSession.id(),genreRequest);
 
     }
 }
--- src/main/java/server/service/genre/GenreService.java
+++ src/main/java/server/service/genre/GenreService.java
@@ -4,26 +4,28 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import server.domain.genre.Genre;
+import server.domain.member.persist.Member;
 import server.mapper.genre.GenreMapper;
 import server.mapper.genre.dto.GenreRequest;
 import server.repository.genre.GenreRepository;
+import server.repository.member.MemberRepository;
 
 @Service
 public class GenreService {
 
+    MemberRepository memberRepository;
     GenreRepository genreRepository;
 
-    public GenreService(GenreRepository genreRepository){
+    public GenreService(MemberRepository memberRepository,GenreRepository genreRepository){
         this.genreRepository = genreRepository;
+        this.memberRepository = memberRepository;
 
     }
 
     @Transactional
-    public void saveUserGenre(GenreRequest genreRequest) throws JsonProcessingException {
+    public void saveUserGenre(final long memberId,GenreRequest genreRequest) throws JsonProcessingException {
 
-        Genre genre = GenreMapper.toEntity(genreRequest);
-
-
+        Genre genre = GenreMapper.toEntity(memberId, genreRequest);
         genreRepository.save(genre);
 
 
