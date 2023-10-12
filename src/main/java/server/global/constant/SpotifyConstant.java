663507b20c5fd0dd1f1e82cdcb9403b52798e733
Jung-YouHwan <yhwjd99@gmail.com>
2023-10-12 18:24:53 +0900

    카카오 위치 서비스 api 구현

--- src/main/java/server/global/constant/SpotifyConstant.java
+++ src/main/java/server/global/constant/SpotifyConstant.java
@@ -7,9 +7,11 @@ public enum SpotifyConstant {
 
     public static String spotifyAccessToken;
     public static final String SPOTIFY_TRACKS_URL = "https://api.spotify.com/v1/search?q=";
+    public static final String KAKAO_LOCATION_KEYWORD_URL = "https://dapi.kakao.com/v2/local/search/keyword";
     public static final String BASIC_CONDITION = "&type=track&limit=30";
     public static final String AUTHORIZATION = "Authorization";
     public static final String BEARER = "Bearer ";
+    public static final String KAKAO_AK = "KakaoAK ";
     public static final String TRACKS = "tracks";
     public static final String ITEMS = "items";
     public static final String NAME = "name";
@@ -17,6 +19,15 @@ public enum SpotifyConstant {
     public static final String ALBUM = "album";
     public static final String IMAGES = "images";
     public static final String URL = "url";
+    public static final String QUERY = "query";
+    public static final String BRACE_QUERY = "{query}";
+    public static final String SORT = "sort";
+    public static final String DISTANCE = "distance";
+    public static final String DOCUMENTS = "documents";
+    public static final String PLACE_NAME = "place_name";
+    public static final String X = "x";
+    public static final String Y = "y";
+    public static final String M = "m";
     public static final int ZERO = 0;
 
     public static void updateToken(final String updateToken) {
