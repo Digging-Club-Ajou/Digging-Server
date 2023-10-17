7d9aec1f553c55cfd2af778c6508e2c141de5842
yeiin <yeini0417@gmail.com>
2023-10-17 14:03:03 +0900

    artist info spotify check

--- src/main/java/server/service/artist/ArtistInfoCreateService.java
+++ src/main/java/server/service/artist/ArtistInfoCreateService.java
@@ -1,41 +1,15 @@
 package server.service.artist;
 
-import com.amazonaws.services.s3.model.CSVInput;
-import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.model.PutObjectRequest;
 import jakarta.transaction.Transactional;
-import org.springframework.batch.core.StepContribution;
-import org.springframework.batch.core.step.item.ChunkProvider;
-import org.springframework.batch.item.Chunk;
-import org.springframework.batch.item.ExecutionContext;
-import org.springframework.batch.item.ItemWriter;
-import org.springframework.batch.item.file.FlatFileItemReader;
-import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
-import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
-import org.springframework.batch.item.file.mapping.DefaultLineMapper;
-import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
-import org.springframework.batch.item.util.FileUtils;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.multipart.MultipartFile;
 import server.domain.artist.ArtistInfo;
-import server.domain.artist.Era;
-import server.global.exception.BadRequestException;
-import server.mapper.artist.dto.ArtistInfoDto;
 import server.repository.artist.ArtistInfoRepository;
 import server.service.spotify.SpotifySearchArtistProdService;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-import static server.global.constant.ExceptionMessage.PROFILES_SAVE_EXCEPTION;
-import static server.global.constant.TextConstant.ALBUM_IMAGE;
-import static server.global.constant.TextConstant.DIGGING_CLUB;
 
 @Service
 public class ArtistInfoCreateService {
@@ -61,15 +35,11 @@ public class ArtistInfoCreateService {
                 String imageURL = spotifySearchArtistProdService.searchArtist(artistInfo.getName());
                 artistInfo.setImageURL(imageURL);
             });
-            System.out.println(artistInfoList.get(0).getImageURL());
-//            artistInfoRepository.saveAll(artistInfoList);
+            artistInfoRepository.saveAll(artistInfoList);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
-//        items.open(new ExecutionContext());
-//
-//        items.getClass();
 
     }
 
