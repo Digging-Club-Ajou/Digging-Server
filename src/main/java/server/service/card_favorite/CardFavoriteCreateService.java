587e5c9bb2462e0e5fe3d26e7252e5837c10ff43
Jung-YouHwan <yhwjd99@gmail.com>
2023-10-29 11:37:24 +0900

    멜로디 카드 좋아요 누르면 알림 목록에 추가

--- src/main/java/server/service/card_favorite/CardFavoriteCreateService.java
+++ src/main/java/server/service/card_favorite/CardFavoriteCreateService.java
@@ -3,19 +3,35 @@ package server.service.card_favorite;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import server.domain.card_favorite.CardFavorite;
+import server.domain.melody_card.MelodyCard;
+import server.domain.member.persist.Member;
 import server.mapper.card_favorite.CardFavoriteMapper;
 import server.mapper.card_favorite.dto.CardFavoriteRequest;
 import server.repository.card_favorite.CardFavoriteRepository;
+import server.repository.melody_card.MelodyCardRepository;
+import server.repository.member.MemberRepository;
+import server.service.notification.NotificationCreateService;
 
 import java.util.Optional;
+
+import static server.global.constant.NotificationConstant.MELODY_CARD_LIKES_NOTIFICATION;
 
 @Service
 public class CardFavoriteCreateService {
 
     private final CardFavoriteRepository cardFavoriteRepository;
+    private final MemberRepository memberRepository;
+    private final MelodyCardRepository melodyCardRepository;
+    private final NotificationCreateService notificationCreateService;
 
-    public CardFavoriteCreateService(final CardFavoriteRepository cardFavoriteRepository) {
+    public CardFavoriteCreateService(final CardFavoriteRepository cardFavoriteRepository,
+                                     final MemberRepository memberRepository,
+                                     final MelodyCardRepository melodyCardRepository,
+                                     final NotificationCreateService notificationCreateService) {
         this.cardFavoriteRepository = cardFavoriteRepository;
+        this.memberRepository = memberRepository;
+        this.melodyCardRepository = melodyCardRepository;
+        this.notificationCreateService = notificationCreateService;
     }
 
     @Transactional
@@ -29,6 +45,17 @@ public class CardFavoriteCreateService {
         } else {
             CardFavorite cardFavorite = CardFavoriteMapper.toEntity(memberId, dto);
             cardFavoriteRepository.save(cardFavorite);
+        }
+
+        notification(memberId, dto);
+    }
+
+    private void notification(final long memberId, final CardFavoriteRequest dto) {
+        if (dto.isLike()) {
+            Member member = memberRepository.getById(memberId);
+            MelodyCard melodyCard = melodyCardRepository.getById(dto.melodyCardId());
+            String notificationMessage = member.getNickname() + MELODY_CARD_LIKES_NOTIFICATION;
+            notificationCreateService.saveNotificationMessage(melodyCard.getMemberId(), notificationMessage);
         }
     }
 }
