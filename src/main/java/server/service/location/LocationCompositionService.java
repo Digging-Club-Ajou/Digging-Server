6a2a02269c4bd97dd6edfa1ba808a0969097fdb7
Jung-YouHwan <yhwjd99@gmail.com>
2023-11-21 09:43:56 +0900

    fix : 장소 검색 정보 반환 보완

--- src/main/java/server/service/location/LocationCompositionService.java
+++ src/main/java/server/service/location/LocationCompositionService.java
@@ -18,13 +18,15 @@ public class LocationCompositionService {
     public List<LocationResponse> findLocation(final String query, final String x, final String y, final int page){
         List<LocationResponse> locationBeforeResponses;
         List<LocationResponse> locationAfterResponses = locationService.findLocation(query, x, y, page);
+        String afterPlaceName = locationAfterResponses.get(0).placeName();
 
         if (page > 1) {
             locationBeforeResponses = locationService.findLocation(query, x, y, page - 1);
-            String beforeDistance = locationBeforeResponses.get(0).placeName();
-            String afterDistance = locationAfterResponses.get(0).placeName();
-            if (beforeDistance.equals(afterDistance)) {
-                return new ArrayList<>();
+            for (LocationResponse locationBeforeResponse : locationBeforeResponses) {
+                String beforePlaceName = locationBeforeResponse.placeName();
+                if (beforePlaceName.equals(afterPlaceName)) {
+                    return new ArrayList<>();
+                }
             }
         }
 
